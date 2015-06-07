2cbe54fe250b9459c83e371e09ca2bf1908b749c
OpenKompani <herlarb@gmail.com>
2015-06-07 16:15:38 +0200

    Testing Out Git On Django

---
+++ DjangoForHeroku/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Herlarby\Documents\Visual Studio 2013\Projects\Python\Django\DjangoForHeroku\DjangoForHeroku\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
